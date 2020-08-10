be44be440691df39f85236cff4d538fb03798ce9
alberthosys <alberthosystemas@gmail.com>
2020-08-10 12:04:12 -0500

    comentarios

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public form:FormGroup;
+  public comentario:string[]=[];
+  constructor(public fb:FormBuilder){
+    this.form=fb.group({
+      comentario:[null]
+    })
   }
 
+  ngOnInit(): void {
+    this.comentario.push("Este es un comentario....")
+  }
+
+  public add(){
+    this.comentario.push(this.form.controls.comentario.value);
+  }
+
+  public delete(pos:number){
+    let tempList:string[]=[];
+    this.comentario.forEach((item ,i)=>{
+      if(i!=pos){
+        tempList.push(item)
+      }
+    })
+    this.comentario=tempList;
+    this.form.controls.comentario.setValue(null);
+  }
 }
